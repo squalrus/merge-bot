485aa656514cb7f1901e07ea8213ee687a15a644
Chad Schulz <chad.awesome@gmail.com>
2019-09-21 14:47:11 -0700

    Add more logging

--- index.js
+++ index.js
@@ -9,8 +9,17 @@ async function run() {
         const time = (new Date()).toDateString();
         core.setOutput('time', time);
 
-        const payload = JSON.stringify(github.context.payload, undefined, 2);
-        console.log(`The event payload: ${payload}`);
+        const payload = github.context.payload;
+        console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`);
+
+        // check if labeled change
+        if (payload.action == 'labeled') {
+            console.log("LABELED");
+        }
+
+        if (payload.action == 'unlabeled') {
+            console.log("UNLABELED");
+        }
 
         // create a GitHub client
         const token = core.getInput('GITHUB_TOKEN');
@@ -18,26 +27,26 @@ async function run() {
 
         // get requested reviewer list
         const reviewers = await octokit.pulls.listReviews({
-            owner: github.context.payload.pull_request.user.login,
-            repo: github.context.payload.repository.name,
-            pull_number: github.context.payload.number
+            owner: payload.pull_request.user.login,
+            repo: payload.repository.name,
+            pull_number: payload.number
         });
 
         console.log(`Reviewers: ${JSON.stringify(reviewers)}`);
 
         const labels = octokit.issues.listLabelsOnIssue({
-            owner: github.context.payload.pull_request.user.login,
-            repo: github.context.payload.repository.name,
-            issue_number: github.context.payload.number
+            owner: payload.pull_request.user.login,
+            repo: payload.repository.name,
+            issue_number: payload.number
         });
 
         console.log(`Labels: ${JSON.stringify(labels)}`);
 
         await octokit.issues.createComment({
             owner: 'squalrus',
-            repo: github.context.payload.repository.name,
-            issue_number: github.context.payload.number,
-            body: `howdy: ${github.context.payload.label.name}`
+            repo: payload.repository.name,
+            issue_number: payload.number,
+            body: `howdy: ${payload.label.name}`
         });
     } catch (error) {
         core.setFailed(error.message);

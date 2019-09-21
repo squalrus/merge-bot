58e6129be1f957b3b5b4781ac644b11270278e7a
Chad Schulz <chad.awesome@gmail.com>
2019-09-21 11:56:29 -0700

    Console log reviewer list

--- index.js
+++ index.js
@@ -10,18 +10,27 @@ async function run() {
         core.setOutput('time', time);
 
         const payload = JSON.stringify(github.context.payload, undefined, 2);
-        // console.log(`The event payload: ${payload}`);
+        console.log(`The event payload: ${payload}`);
 
+        // create a GitHub client
         const token = core.getInput('token');
         const octokit = new github.GitHub(token);
 
-        const { data: comment } = await octokit.issues.createComment({
+        // get requested reviewer list
+        const reviewers = await octokit.listReviewRequests({
+            owner: github.context.payload.pull_request.owner,
+            repo: github.context.payload.repository.name,
+            pull_number: github.context.payload.number
+        });
+
+        console.log(`Reviewers: ${reviewers}`);
+
+        await octokit.issues.createComment({
             owner: 'squalrus',
             repo: github.context.payload.repository.name,
             issue_number: github.context.payload.number,
             body: `howdy: ${github.context.payload.label.name}`
         });
-        console.log(comment);
     } catch (error) {
         core.setFailed(error.message);
     }

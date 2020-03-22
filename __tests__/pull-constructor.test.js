1083cc7ea933ace2ec2baa32deccb3461da88f8f
Chad Schulz <chad.awesome@gmail.com>
2020-03-21 23:27:34 -0700

    Trigger update and refactor (#26)

--- __tests__/pull-constructor.test.js
+++ __tests__/pull-constructor.test.js
@@ -3,10 +3,32 @@ const Pull = require('../lib/pull');
 const payloadDefault = require('../__mocks__/pull/payload-default');
 const payloadDefaultExpected = require('../__mocks__/pull/payload-default-expected');
 
+const payloadPullRequest = require('../__mocks__/pull_request/payload');
+const payloadPullRequestExpected = require('../__mocks__/pull/payload-pull_request-expected');
+
+const payloadPullRequestReview = require('../__mocks__/pull_request_review/payload');
+const payloadPullRequestReviewExpected = require('../__mocks__/pull/payload-pull_request_review-expected');
+
 test('pull constructor, default settings', () => {
     const pull = new Pull(payloadDefault);
     const received = JSON.stringify(pull);
     const expected = JSON.stringify(payloadDefaultExpected);
+
+    expect(received).toBe(expected);
+});
+
+test('pull constructor, pull_request payload', () => {
+    const pull = new Pull(payloadPullRequest);
+    const received = JSON.stringify(pull);
+    const expected = JSON.stringify(payloadPullRequestExpected);
+
+    expect(received).toBe(expected);
+});
+
+test('pull constructor, pull_request_review payload', () => {
+    const pull = new Pull(payloadPullRequestReview);
+    const received = JSON.stringify(pull);
+    const expected = JSON.stringify(payloadPullRequestReviewExpected);
 
     expect(received).toBe(expected);
 });

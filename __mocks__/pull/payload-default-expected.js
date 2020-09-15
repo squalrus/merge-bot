61583bce875e2ef7bcacdbf92599c42f485d261c
Chad Schulz <chad.awesome@gmail.com>
2020-09-14 21:46:44 -0700

    Add support for forks
    Add the sha as the ref rather than building a string representing the branch name
    
    > The ref can be a SHA, branch name, or a tag name.
    
    From https://developer.github.com/v3/checks/runs/#list-check-runs-for-a-git-reference
    
    fixes #40

--- __mocks__/pull/payload-default-expected.js
+++ __mocks__/pull/payload-default-expected.js
@@ -1,9 +1,9 @@
 module.exports = {
     "labels": ["foo", "bar", "ready"],
     "owner": "squalrus",
-    "pull_number": "20",
+    "pull_number": 20,
     "reviews": [],
-    "ref": "heads/master",
+    "ref": "1234724d27c4fae27b402212182b64fda77040b5",
     "repo": "merge-bot",
     "requested_reviewers": ["squalrus", "timgrove"],
     "checks": {}
--- __mocks__/pull/payload-pull_request-expected.js
+++ __mocks__/pull/payload-pull_request-expected.js
@@ -3,7 +3,7 @@ module.exports = {
     "owner": "Codertocat",
     "pull_number": 2,
     "reviews": [],
-    "ref": "heads/changes",
+    "ref": "ec26c3e57ca3a959ca5aad62de7213c562f8c821",
     "repo": "Hello-World",
     "requested_reviewers": [],
     "checks": {}

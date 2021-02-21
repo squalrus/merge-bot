312ea72749e149453923f50288c86b352b20b3f4
Chad Schulz <chad.awesome@gmail.com>
2021-02-20 21:19:29 -0800

    Update ref to `heads/branch_name` rather than `sha`

--- __mocks__/pull/payload-fork-expected.js
+++ __mocks__/pull/payload-fork-expected.js
@@ -3,7 +3,7 @@ module.exports = {
     "owner": "squalrus",
     "pull_number": 36,
     "reviews": [],
-    "ref": "05ca724d27c4fae27b402212182b64fda77040b5",
+    "ref": "heads/optional-delete-branch",
     "repo": "merge-bot",
     "requested_reviewers": [],
     "checks": {}
--- __mocks__/pull/payload-pull_request_review-expected.js
+++ __mocks__/pull/payload-pull_request_review-expected.js
@@ -3,7 +3,7 @@ module.exports = {
     "owner": "Codertocat",
     "pull_number": 2,
     "reviews": [],
-    "ref": "ec26c3e57ca3a959ca5aad62de7213c562f8c821",
+    "ref": "heads/changes",
     "repo": "Hello-World",
     "requested_reviewers": [],
     "checks": {}

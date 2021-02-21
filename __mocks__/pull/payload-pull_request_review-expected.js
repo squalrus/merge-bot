88d79b65066d0a16140ab8469904dca0caf3696c
Chad Schulz <chad.awesome@gmail.com>
2021-02-20 22:11:49 -0800

    Pass branch name for `listForRef` (#50)

--- __mocks__/pull/payload-pull_request_review-expected.js
+++ __mocks__/pull/payload-pull_request_review-expected.js
@@ -3,6 +3,7 @@ module.exports = {
     "owner": "Codertocat",
     "pull_number": 2,
     "reviews": [],
+    "branch_name":"changes",
     "ref": "heads/changes",
     "repo": "Hello-World",
     "requested_reviewers": [],

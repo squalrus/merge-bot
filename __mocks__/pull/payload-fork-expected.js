562278fc2c3fa786a1d530ab9036f32119fcc3ec
Chad Schulz <chad.awesome@gmail.com>
2021-04-24 15:50:07 -0700

    Update ref to `sha` from `heads/branch_name` (#55)
    fixes #54

--- __mocks__/pull/payload-fork-expected.js
+++ __mocks__/pull/payload-fork-expected.js
@@ -4,7 +4,7 @@ module.exports = {
     "pull_number": 36,
     "reviews": [],
     "branch_name": "optional-delete-branch",
-    "ref": "heads/optional-delete-branch",
+    "ref": "05ca724d27c4fae27b402212182b64fda77040b5",
     "repo": "merge-bot",
     "requested_reviewers": [],
     "checks": {},

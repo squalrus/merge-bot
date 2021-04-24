2bc4ff2018db2ec40da2132dc22868890f0eed12
Chad Schulz <chad.awesome@gmail.com>
2021-04-24 15:39:34 -0700

    Update ref to `sha` from `heads/branch_name`
    fixes #54

--- __mocks__/pull/payload-default-expected.js
+++ __mocks__/pull/payload-default-expected.js
@@ -4,7 +4,7 @@ module.exports = {
     "pull_number": 20,
     "reviews": [],
     "branch_name": "master",
-    "ref": "heads/master",
+    "ref": "1234724d27c4fae27b402212182b64fda77040b5",
     "repo": "merge-bot",
     "requested_reviewers": ["squalrus", "timgrove"],
     "checks": {},
--- __mocks__/pull/payload-pull_request-expected.js
+++ __mocks__/pull/payload-pull_request-expected.js
@@ -4,7 +4,7 @@ module.exports = {
     "pull_number": 2,
     "reviews": [],
     "branch_name": "changes",
-    "ref": "heads/changes",
+    "ref": "ec26c3e57ca3a959ca5aad62de7213c562f8c821",
     "repo": "Hello-World",
     "requested_reviewers": [],
     "checks": {},
